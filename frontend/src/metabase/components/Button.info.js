653f4e294be26d1246503bf2458e406281a44e73
Kyle Doherty <kyle.l.doherty@gmail.com>
2019-05-27 12:55:27 -0700

    update examples

--- frontend/src/metabase/components/Button.info.js
+++ frontend/src/metabase/components/Button.info.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from "metabase/components/Button";
-import { t } from "ttag";
 export const component = Button;
 
 export const description = `
@@ -8,7 +7,7 @@ Metabase's main button component.
 `;
 
 export const examples = {
-  "": <Button>{t`Clickity click`}</Button>,
-  primary: <Button primary>{t`Clickity click`}</Button>,
-  "with an icon": <Button icon="star">{t`Clickity click`}</Button>,
+  "": <Button>{`Clickity click`}</Button>,
+  primary: <Button variant="primary">{`Clickity click`}</Button>,
+  danger: <Button variant="danger">{`No clickity click`}</Button>,
 };
